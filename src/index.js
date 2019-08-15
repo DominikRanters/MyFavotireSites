0ee6c13b07ed701d8fdeb5b8224e16403eb7fbf7
Dominik Ranters <dominik.ranters@tobit.cm>
2019-08-15 18:09:46 +0200

    Add Formular

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './app.scss'
+
+class Tappproject {
+    constructor() {
+
+    }
+
+
+}
+
+chayns.ready.then(() => {
+    chayns.ui.initAll();
+    new Tapproject();
+}).catch((error) => console.log("No Chayns", error));
