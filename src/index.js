2b5a75a65e985f93e0d6751cb24de6fd538146db
Dominik Ranters <dominik.ranters@tobit.cm>
2019-08-16 10:08:20 +0200

    :sparkles: fetch jason

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './app.scss'
 
-class Tappproject {
+class Tapproject {
     constructor() {
-
+        fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                console.log('parsed json', json)
+            }).catch(function(ex) {
+                console.log('parsing failed', ex)
+            })
     }
 
 
