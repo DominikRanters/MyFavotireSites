04b0ed097b3444085d5c3cdeac3b0538b7142747
Dominik Ranters <dominik.ranters@tobit.cm>
2019-08-16 10:42:33 +0200

    Get json data

--- src/index.js
+++ src/index.js
@@ -6,17 +6,32 @@ class Tapproject {
             .then(function(response) {
                 return response.json()
             }).then(function(json) {
-                console.log('parsed json', json)
-                this._getData(json);
+
+
+                const jsonData = json.Data;
+                let sitesList = '';
+
+                console.log('parsed json', json.Data)
+
+                for (let i = 0; i < data.length; i++) {
+                    sitesList +=
+                        '<div class="list-item list-item--clickable">' +
+                        '<div class="list-item__header">' +
+                        '<div class="list-item__image" style="background-image: url(&quot;https://chayns.tobit.com/storage/' + jsonData.siteId + '/Images/icon-72.png&quot;);"></div>' +
+                        '<div class="list-item__titles">' +
+                        '<div class="list-item__title ellipsis">' + jsonData.appstoreName + '</div>' +
+                        '</div>' +
+                        '<div class="list-item__spacer"></div>' +
+                        '</div>' +
+                        '</div>'
+
+
+                    console.log(data[i].siteId);
+                }
+
             }).catch(function(ex) {
                 console.log('parsing failed', ex)
             })
-    }
-
-    _getData(json) {
-        for (let i = 0; i < json.Data.Length; i++) {
-            console.log(i);
-        }
     }
 
 
