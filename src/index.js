eb799d97584f1b6fcbb28fdf6e77f5f07ba62ab3
Dominik Ranters <dominik.ranters@tobit.cm>
2019-08-19 10:19:06 +0200

    :art:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 import './app.scss'
 
 const $list = document.querySelector('.sitesList');
@@ -11,74 +13,18 @@ const http = new XMLHttpRequest();
 
 let url;
 
+
+chayns.ready.then(() => {
+    chayns.ui.initAll();
+    new Tapproject();
+}).catch((error) => console.log("No Chayns", error))
+
+
 class Tapproject {
+
     constructor() {
-
-
         this._eventlistner();
         this._fetchUrl('love');
-
-
-    }
-
-    _fetchUrl(searchValue) {
-
-        url = 'https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=' + searchValue + '&Skip=0&Take=50'
-
-        fetch(url)
-            .then(function(response) {
-                return response.json()
-            }).then(function(json) {
-
-                console.log(json.Data.length !== $list.childElementCount)
-
-                if (json.Data.length !== $list.childElementCount) {
-
-                    if ($list.childElementCount > 0) {
-                        while ($list.firstChild) {
-                            $list.removeChild($list.firstChild);
-                        }
-                    }
-
-
-                    for (let item of json.Data) {
-
-                        let listItem = document.createElement("div");
-                        listItem.className = "list-item list-item--clickable";
-                        let listItemHeader = document.createElement("div");
-                        listItemHeader.className = 'list-item__header';
-                        let listItemTitles = document.createElement("div");
-                        listItemTitles.className = 'list-item__titles';
-                        let listItemImage = document.createElement('div');
-                        listItemImage.className = 'list-item__image';
-                        let listItemTitle = document.createElement('div');
-                        listItemTitle.className = 'list-item__title ellipsis';
-
-                        try {
-                            http.open('HEAD', `https://chayns.tobit.com/storage/${item.siteId}/Images/icon-72.png`, false);
-                            http.send();
-                            listItemImage.style.backgroundImage = `url(https://chayns.tobit.com/storage/${item.siteId}/Images/icon-72.png)`;
-                        } catch {
-                            listItemImage.style.backgroundImage = `url(https://chayns.tobit.com/storage/75508-15270/Images/icon-72.png)`;
-                        }
-
-
-                        let listItemTitleNode = document.createTextNode(item.appstoreName);
-
-                        listItemTitle.appendChild(listItemTitleNode);
-                        listItemTitles.appendChild(listItemTitle);
-                        listItemHeader.appendChild(listItemImage);
-                        listItemHeader.appendChild(listItemTitles);
-                        listItem.appendChild(listItemHeader);
-                        $list.appendChild(listItem);
-
-                        listItem.addEventListener("click", () => chayns.openUrlInBrowser(`http://chayns.net/${item.siteId}/`));
-                    }
-                }
-
-            }).catch(function(ex) {
-                console.log('parsing failed', ex)
-            })
     }
 
     _eventlistner() {
@@ -93,9 +39,9 @@ class Tapproject {
             chayns.dialog.alert('', "Die Email exestiert nicht.");
         } else if ($Url.value.indexOf('.') === -1) {
             chayns.dialog.alert('', "Keine gültige Url");
-        } else if ($Name.value !== '' && $Url.value !== '' && $eMail.value !== '') {
+        } else if ($Name.value !== '' && $Url.value !== '' && $eMail.value !== '' && $Kommentar.value !== '') {
             chayns.intercom.sendMessageToPage({
-                text: 'Hier ist meine Seite! Name: ' + $Name.value + '. Url: ' + $Url.value + '. eMail: ' + $eMail.value + '. Kommentar: ' + $Kommentar.value + '.'
+                text: `Hier ist meine Seite! Name:${$Name.value}. Url: ${$Url.value}. eMail: ${$eMail.value}. Kommentar: ${$Kommentar.value}.`
             }).then(function(data) {
                 if (data.status == 200) {
                     $Name.value = '';
@@ -104,12 +50,84 @@ class Tapproject {
                     $Kommentar.value = '';
                     chayns.dialog.alert('', 'Ihre Seite wird geprüft und anschließend hinzugefügt');
                 }
+            })
+        } else if ($Name.value !== '' && $Url.value !== '' && $eMail.value !== '') {
+            chayns.intercom.sendMessageToPage({
+                text: `Hier ist meine Seite! Name:${$Name.value}. Url: ${$Url.value}. eMail: ${$eMail.value}.`
+            }).then(function(data) {
+                if (data.status == 200) {
+                    $Name.value = '';
+                    $Url.value = '';
+                    $eMail.value = '';
+                    chayns.dialog.alert('', 'Ihre Seite wird geprüft und anschließend hinzugefügt');
+                }
             });
         }
     }
-}
 
-chayns.ready.then(() => {
-    chayns.ui.initAll();
-    new Tapproject();
-}).catch((error) => console.log("No Chayns", error))
+    _fetchUrl(searchValue) {
+
+        url = `https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=${searchValue}&Skip=0&Take=50`
+
+        fetch(url)
+            .then((response) => {
+                return response.json()
+            }).then((json) => {
+                this._fillSiteList(json.Data);
+            }).catch((ex) => {
+                console.log(PARSING_FAILED, ex)
+            })
+
+    }
+
+    _fillSiteList(allData) {
+
+        if (allData.length !== $list.childElementCount) {
+
+            if ($list.childElementCount > 0) {
+                while ($list.firstChild) {
+                    $list.removeChild($list.firstChild);
+                }
+            }
+
+
+            for (let data of allData) {
+
+                let listItem = document.createElement("div");
+                listItem.className = "list-item list-item--clickable";
+                let listItemHeader = document.createElement("div");
+                listItemHeader.className = 'list-item__header';
+                let listItemTitles = document.createElement("div");
+                listItemTitles.className = 'list-item__titles';
+                let listItemImage = document.createElement('div');
+                listItemImage.className = 'list-item__image';
+                let listItemTitle = document.createElement('div');
+                listItemTitle.className = 'list-item__title ellipsis';
+
+                try {
+                    http.open('HEAD', `https://chayns.tobit.com/storage/${data.siteId}/Images/icon-72.png`, false);
+                    http.send();
+                    listItemImage.style.backgroundImage = `url(https://chayns.tobit.com/storage/${data.siteId}/Images/icon-72.png)`;
+                } catch {
+                    listItemImage.style.backgroundImage = `url(https://chayns.tobit.com/storage/75508-15270/Images/icon-72.png)`;
+                }
+
+
+                let listItemTitleNode = document.createTextNode(item.appstoreName);
+
+                listItemTitle.appendChild(listItemTitleNode);
+                listItemTitles.appendChild(listItemTitle);
+                listItemHeader.appendChild(listItemImage);
+                listItemHeader.appendChild(listItemTitles);
+                listItem.appendChild(listItemHeader);
+                $list.appendChild(listItem);
+
+                listItem.addEventListener("click", () => chayns.openUrlInBrowser(`http://chayns.net/${data.siteId}/`));
+            }
+        }
+    }
+
+
+
+
+}
